72d6c8a3667d57ae4e5aed6b4048ed1b1f0d61ac
umer107 <umarwaheedlog@gmail.com>
2017-08-22 08:15:01 +0500

    map

---
+++ app/src/main/java/com/umer/andriod/mapactivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.umer.andriod.mapactivity;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity   implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager() .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device.
+     * This method will only be triggered once the user has installed
+     Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng TutorialsPoint = new LatLng(21, 57);
+        mMap.addMarker(new
+                MarkerOptions().position(TutorialsPoint).title("Tutorialspoint.com"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TutorialsPoint));
+    }
+
+
+}
